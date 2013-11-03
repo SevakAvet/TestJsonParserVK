6bea858f8ac11ad7c1f5fb29e65bc8b8222d1d6d
Avetisyan Sevak <Sevak_Avet@mail.ru>
2013-11-03 20:30:17 +0400

    Getting lyrics (song text)

--- src/Controller.java
+++ src/Controller.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,9 +15,11 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
@@ -37,13 +40,19 @@ public class Controller implements Initializable {
 	private static TextField parseField;
 	@FXML
 	private static ListView<String> listView;
+	@FXML
+	private static TextArea songText;
 	
 	private static WebEngine engine;
 	
 	private static final String urlParameters = "authorize?client_id=3972445&scope=8&redirect_uri=http://oauth.vk.com/blank.html&display=popup&response_type=token";
 	private static final String auth = "https://oauth.vk.com/" + urlParameters;
 	
+	private static final String audioSearch = "https://api.vk.com/method/audio.search?q=";
+	private static final String lyricsSearch = "https://api.vk.com/method/audio.getLyrics?";
 	private static String accessToken = null;
+	
+	private static String json;
 	
 	private static int count = 0;
 	
@@ -79,29 +88,50 @@ public class Controller implements Initializable {
 		inputField.setOnKeyReleased(new EventHandler<KeyEvent>() {
 			@Override
 			public void handle(KeyEvent e) {
-				execute(e);
+				executeSearch(e);
 			}
 		});
 		
 		parseField.setOnKeyReleased(new EventHandler<KeyEvent>() {
 			@Override
 			public void handle(KeyEvent e) {
-				execute(e);
+				executeSearch(e);
 			}
 		});
 		
 		countField.setOnKeyReleased(new EventHandler<KeyEvent>() {
 			@Override
 			public void handle(KeyEvent e) {
-				execute(e);
+				executeSearch(e);
+			}
+		});
+		
+		listView.setOnMouseReleased(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent e) {
+				
+				try {
+					ObservableList<String> parsed = parseJson(json, "lyrics_id");
+					String json = executeLyrics(parsed.get(listView.getSelectionModel().getSelectedIndex()));
+					
+					JSONParser jsonParser = new JSONParser();
+					JSONObject jsonObject = (JSONObject) jsonParser.parse(json);
+					jsonObject = (JSONObject) jsonObject.get("response");
+					
+					String text = (String) jsonObject.get("text");
+					songText.setText(text);
+				} catch (ParseException e1) {
+					e1.printStackTrace();
+				}
+				
 			}
 		});
 		
 	}
 	
-	private static void execute(KeyEvent e) {
+	private static void executeSearch(KeyEvent e) {
 		if(e.getCode() == KeyCode.ENTER) {
-			engine.load("https://api.vk.com/method/audio.search?q=" + inputField.getText() + "&auto_complete=1&sort=2&count=" + countField.getText().trim() +"&" + accessToken);
+			engine.load(audioSearch + inputField.getText() + "&auto_complete=1&sort=2&count=" + countField.getText().trim() +"&" + accessToken);
 			
 			try {
 				URL url = new URL(engine.getLocation());
@@ -109,9 +139,9 @@ public class Controller implements Initializable {
 				
 				if(connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
 					BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-					String line = reader.readLine();	
+					json = reader.readLine();	
 					
-					ObservableList<String> parsed = parseJson(line, parseField.getText().trim());
+					ObservableList<String> parsed = parseJson(json, parseField.getText().trim());
 					listView.setItems(parsed);
 				}
 				
@@ -119,6 +149,29 @@ public class Controller implements Initializable {
 				e1.printStackTrace();
 			}
 		}
+	}
+	
+	private static String executeLyrics(String id) {
+		engine.load(lyricsSearch + "lyrics_id=" + id +"&" + accessToken);
+		StringBuilder sb = new StringBuilder();
+		
+		try {
+			URL url = new URL(engine.getLocation());
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			
+			if(connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+				BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+				String line = "";
+				while((line = reader.readLine()) != null) {
+					sb.append(line);
+				}
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return sb.toString();
 	}
 	
 	private static ObservableList<String> parseJson(String json, String parameter) throws ParseException {
